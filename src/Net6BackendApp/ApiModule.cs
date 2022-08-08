4040fd112c9d1badb0e963cd0604f21f4e79f914
Hiroyuki Hasebe <hiroyha@microsoft.com>
2022-08-08 14:32:14 +0900

    initial commit

---
+++ src/Net6BackendApp/ApiModule.cs
@@ -0,0 +1,18 @@
+﻿namespace Net6BackendApp;
+
+public static class ApiModule
+{
+    public static IEndpointRouteBuilder MapApiRoutes(this WebApplication app)
+    {
+        app.MapGet("api/v1/gc", () =>
+        {
+            app.Logger.LogInformation("gc");
+            GC.Collect();
+            return "Ok";
+        })
+        .Produces<string>()
+        .WithName("GC");
+
+        return app;
+    }
+}
---
+++ src/Net6BackendApp/Program.cs
@@ -0,0 +1,21 @@
+using Net6BackendApp;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpLogging();
+app.MapApiRoutes();
+app.MapHealthChecks("/healthz");
+
+app.Run();
